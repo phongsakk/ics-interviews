6236c1406d31b2fb4fa2b7dab5a4e91c4f320d3c
Phongsakk <phongsak.ysd@gmail.com>
2022-09-28 20:00:19 +0700

    fix

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,9 +1,9 @@
-import { ZodPlaceJson } from '../types';
+import { PlaceInfo } from '../types';
 import data from './example_data.json';
 
 interface IResponse {
   status: number;
-  data?: ZodPlaceJson[];
+  data?: PlaceInfo[];
 }
 
 export const Fetch = async (
@@ -11,7 +11,7 @@ export const Fetch = async (
   query: string
 ): Promise<IResponse> => {
   try {
-    const zodData = data.map((item) => ZodPlaceJson.parse(item));
+    const zodData = data.map((item) => PlaceInfo.parse(item));
     const chkCategory = zodData.filter((item) => item.categories.includes(category))
     const chkQuery = chkCategory.filter(item => item.name.toLowerCase().includes(query.toLowerCase()))
     return {
